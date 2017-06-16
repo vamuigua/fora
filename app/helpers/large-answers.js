6a590af598a1c06974bb28364a67c8e47b9ed161
Victor Allen <vamuigua@gmail.com>
2017-06-16 17:22:15 +0300

    Added action to display for no answers and answers below 5

--- app/helpers/large-answers.js
+++ app/helpers/large-answers.js
@@ -2,11 +2,14 @@ import Ember from 'ember';
 
 export function largeAnswers(params) {
   var question = params[0];
-  if(question.get('answers').get('length')>=5){
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-plus"></span>');
+  var answersTotal = question.get('answers').get('length');
+  if(answersTotal>=5){
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-plus"></span>&nbsp;&nbsp;'+answersTotal + ' Answers');
   }else if(question.get('answers').get('length')===0){
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-folder-open"></span>');
-  }
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-folder-open"></span>&nbsp;&nbsp;'+ answersTotal + ' Answers');
+  }else if(question.get('answers').get('length')>0){
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-folder-open"></span>&nbsp;&nbsp;'+ answersTotal + ' Answers');
+}
 }
 
 export default Ember.Helper.helper(largeAnswers);
