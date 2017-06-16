245373ff1716958255a46da8690171e68fc1d74e
Victor Allen <vamuigua@gmail.com>
2017-06-16 15:18:34 +0300

    Added answeredDate object to pass current date

--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -13,7 +13,8 @@ export default Ember.Component.extend({
       var params = {
         author: this.get('author'),
         content: this.get('content'),
-        question: this.get('question')
+        question: this.get('question'),
+        answeredDate: new Date(),
       };
       this.set('addNewAnswer', false);
       this.sendAction('saveAnswer', params);
