6ba7248e28029325689767613555be5b4d92a792
Victor Allen <vamuigua@gmail.com>
2017-06-16 16:05:10 +0300

    Added else action to return a folder icon when no answers have been given to a questuion

--- app/helpers/large-answers.js
+++ app/helpers/large-answers.js
@@ -4,6 +4,8 @@ export function largeAnswers(params) {
   var question = params[0];
   if(question.get('answers').get('length')>=5){
     return Ember.String.htmlSafe('<span class="glyphicon glyphicon-plus"></span>');
+  }else if(question.get('answers').get('length')===0){
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-folder-open"></span>');
   }
 }
 
