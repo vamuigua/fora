0cfd52e6b0fb7783f238ad7f7368cb3edac534aa
Victor Allen <vamuigua@gmail.com>
2017-06-16 14:56:01 +0300

    added a createDate attribute to my question model

--- app/models/question.js
+++ app/models/question.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   author: DS.attr(),
   content:DS.attr(),
-  answers: DS.hasMany('answer', { async: true })
+  answers: DS.hasMany('answer', { async: true }),
+  createDate: DS.attr('date')
 });
