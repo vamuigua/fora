a5a1718bac46e098ffc0787f0d94e38c77833dda
Victor Allen <vamuigua@gmail.com>
2017-06-17 13:04:49 +0300

    Commented out upvote action

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -4,6 +4,12 @@ export default Ember.Component.extend({
   actions: {
     update(question, params) {
       this.sendAction('update', question, params);
-    }
+    },
+    // upVote(question){
+    //   var votes = parseInt(question.get('totalVotes'));
+    //   votes++;
+    //   question.set('totalVotes', votes);
+    //   this.sendAction('upVote1', question);
+    // },
   }
 });
